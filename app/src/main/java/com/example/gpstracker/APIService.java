9b96bcb0c2b6b597b2e59b2657956cfd91198d23
koskakos <fafsdfas93@mail.ru>
2023-04-24 13:18:05 +0600

    deleted LocationBody.java fixed permissions code refactoring

--- app/src/main/java/com/example/gpstracker/APIService.java
+++ app/src/main/java/com/example/gpstracker/APIService.java
@@ -1,35 +1,21 @@
 package com.example.gpstracker;
 
+import android.location.Location;
+
 import java.time.LocalDateTime;
 
 import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface APIService {
     @FormUrlEncoded
-    @POST("test")
-    Call<LocationBody> test(@Field("longitude") double longitude);
-
-
-    @FormUrlEncoded
-    @POST("test1")
-    Call<LocationBody> test1(@Field("longitude") double longitude,
-                             @Field("latitude") double latitude,
-                             @Field("altitude") double altitude,
-                             @Field("speed") double speed,
-                             @Field("bearing") double bearing);
-
-
-    @FormUrlEncoded
     @POST("save")
-    Call<LocationBody> save(@Field("longitude") double longitude,
-                            @Field("latitude") double latitude,
-                            @Field("altitude") double altitude,
-                            @Field("speed") double speed,
-                            @Field("bearing") double bearing,
-                            @Field("date") LocalDateTime date);
+    Call<Location> save(@Field("longitude") double longitude,
+                        @Field("latitude") double latitude,
+                        @Field("altitude") double altitude,
+                        @Field("speed") double speed,
+                        @Field("bearing") double bearing,
+                        @Field("date") LocalDateTime date);
 }
--- app/src/main/java/com/example/gpstracker/MainActivity.java
+++ app/src/main/java/com/example/gpstracker/MainActivity.java
@@ -7,14 +7,12 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.icu.util.Calendar;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,11 +22,7 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -53,7 +47,9 @@ public class MainActivity extends AppCompatActivity {
     private Retrofit retrofit;
     private APIService api;
 
-    private LocationBody lastSentLocation;
+    private Location lastSentLocation;
+    private LocalDateTime lastSentDateTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,25 +61,29 @@ public class MainActivity extends AppCompatActivity {
         tvSpeed = findViewById(R.id.speed);
         tvBearing = findViewById(R.id.bearing);
         swLocationUpdates = findViewById(R.id.locationUpdatesSwitch);
-
         locationRequest = new LocationRequest.Builder(1000).
                 setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .build();
-
+        if(!isHavePermissions()) requestPermissions();
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 Location currentLocation = locationResult.getLastLocation();
                 updateUI(currentLocation);
-                if(lastSentLocation == null) {
-                    lastSentLocation = save(currentLocation);
+                if (lastSentLocation == null) {
+                    lastSentDateTime = save(currentLocation);
+                    lastSentLocation = currentLocation;
                 } else {
-                    if(Math.abs(lastSentLocation.getLocation().getBearing() - currentLocation.getBearing()) >= 10 ||
-                        lastSentLocation.getLocation().distanceTo(currentLocation) >= 200 ||
-                        Math.abs(lastSentLocation.getDate().toEpochSecond(ZoneOffset.UTC) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) >= 600) {
-                        lastSentLocation = save(currentLocation);
-                    }
+                    if (Math.abs(lastSentLocation.getBearing() - currentLocation.getBearing()) >= 10) {
+                        Toast.makeText(MainActivity.this, "Bearing", Toast.LENGTH_LONG).show();
+                    } else if (lastSentLocation.distanceTo(currentLocation) >= 200) {
+                        Toast.makeText(MainActivity.this, "Distance", Toast.LENGTH_LONG).show();
+                    } else if (Math.abs(lastSentDateTime.toEpochSecond(ZoneOffset.UTC) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) >= 600) {
+                        Toast.makeText(MainActivity.this, "Time", Toast.LENGTH_LONG).show();
+                    } else return;
+                    lastSentDateTime = save(currentLocation);
+                    lastSentLocation = currentLocation;
                 }
             }
         };
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         swLocationUpdates.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(swLocationUpdates.isChecked()) {
+                if (swLocationUpdates.isChecked()) {
                     startLocationUpdates();
                 } else {
                     stopLocationUpdates();
@@ -99,51 +99,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "granted", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, "not granted", Toast.LENGTH_LONG).show();
         }
 
-        updateGps();
     }
-    public LocationBody save(Location location) {
+
+    public LocalDateTime save(Location location) {
         LocalDateTime localDateTime = LocalDateTime.now();
-        //Toast.makeText(this, localDateTime.toString(), Toast.LENGTH_LONG).show();
-        if(retrofit == null || api == null) {
+        if (retrofit == null || api == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl("https://gpstracker.herokuapp.com/")
                     .addConverterFactory(GsonConverterFactory.create()).build();
 
             api = retrofit.create(APIService.class);
         }
-        Call<LocationBody> call = api.save(location.getLongitude(), location.getLatitude(),
+        Call<Location> call = api.save(location.getLongitude(), location.getLatitude(),
                 location.getAltitude(), location.getSpeed(), location.getBearing(), localDateTime);
-        call.enqueue(new Callback<LocationBody>() {
+        call.enqueue(new Callback<Location>() {
             @Override
-            public void onResponse(Call<LocationBody> call, Response<LocationBody> response) {
-                //tvBearing.setText(response.message());
-                //tvBearing.setText(response.code());
-//                LocationBody responseFromAPI = response.body();
-//                Toast.makeText(MainActivity.this, String.valueOf(responseFromAPI.longitude), Toast.LENGTH_LONG).show();
-                //tvBearing.setText(String.valueOf(response.body().longitude));
-                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
+            public void onResponse(Call<Location> call, Response<Location> response) {
+
             }
 
             @Override
-            public void onFailure(Call<LocationBody> call, Throwable t) {
-                //tvBearing.setText(t.getLocalizedMessage());
-                //Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            public void onFailure(Call<Location> call, Throwable t) {
 
             }
         });
 
-        return new LocationBody(location, localDateTime);
+        return localDateTime;
     }
+
+    public boolean isGeoDisabled() {
+        LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean mIsGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean mIsNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean mIsGeoDisabled = !mIsGPSEnabled && !mIsNetworkEnabled;
+        return mIsGeoDisabled;
+    }
+
     @SuppressLint("MissingPermission")
     private void startLocationUpdates() {
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        if(isGeoDisabled()) startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
         updateGps();
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
     }
 
     private void stopLocationUpdates() {
@@ -159,30 +161,28 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == PERMISSION_FINE_LOCATION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (!(requestCode == PERMISSION_FINE_LOCATION && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            Toast.makeText(this, "No GPS permissions", Toast.LENGTH_LONG).show();
             updateGps();
-        } else {
-            Toast.makeText(this, "No GPS permissions", Toast.LENGTH_SHORT).show();
-            //finish();
         }
+    }
 
+    private boolean isHavePermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, PERMISSION_FINE_LOCATION);
     }
 
     private void updateGps() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    updateUI(location);
-                    //save(location);
-                }
-            });
+        if (isHavePermissions()) {
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         } else {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, PERMISSION_FINE_LOCATION);
+            requestPermissions();
         }
-
     }
 
     private void updateUI(Location location) {
